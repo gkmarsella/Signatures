9a85a7963f6ba7f5d4a7f5d965e9d29afdf6ffae
Greg <gkmarsella@gmail.com>
2020-05-07 18:53:25 -0400

    styled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Signature from './Signature';
 import MobileSignature from './MobileSignature';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,101 +12,101 @@ class App extends React.Component {
       title: '',
       mobile: '',
       office: '',
+      fax: '',
       email: '',
       city: '',
       state: '',
       isa: '',
-      additional: '',
-      numbers: {
-        mobile: '',
-        office: '',
-        fax: '',
-        other: ''
-      }
+      additional: ''
     }
   }
 
-
-onChange = (event) => {
-  const targetName = event.target.name;
-  const oldState = this.state;
-  this.setState({
-    ...oldState,
-    [targetName]: event.target.value
-  })
-}
-
-
-
+  onChange = (event) => {
+    const targetName = event.target.name;
+    const oldState = this.state;
+    this.setState({
+      ...oldState,
+      [targetName]: event.target.value
+    })
+  }
 
   render() {
-    return(
+    return (
       <div class="wrapper">
-        <div class="user-display d-flex justify-content-center">
+
+        <div class="user-display">
           <div class='form-container'>
             <form>
-              <input
-                class="form-control" 
-                name="name" 
-                type="text" 
-                value={this.state.value} 
-                onChange={this.onChange}
-                placeholder="Full Name"
-                required />
-            
-              <input
-                class="form-control" 
-                name="title" 
-                type="text" 
-                value={this.state.value} 
-                onChange={this.onChange}
-                placeholder="Title"
-                required />
-              <input
-                class="form-control" 
-                name="mobile"
-                type="tel" 
-                value={this.state.value} 
-                onChange={this.onChange}
-                placeholder="Mobile number"
-                required />
-                <small class="form-text text-muted">
-                Enter 10 digit number without any punctuation 
-                </small>
-              <input
-                class="form-control" 
-                name="office" 
-                type="tel" 
-                value={this.state.value} 
-                onChange={this.onChange}
-                placeholder="Office number"
-                required />
-                <small class="form-text text-muted">
-                Enter 10 digit number without any punctuation 
-                </small>
+              <div class="line-1 input-box">
+                <input
+                  class="form-control mb-3"
+                  name="name"
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.onChange}
+                  placeholder="Full Name"
+                  required />
 
-              <input
-                class="form-control" 
-                name="email" 
-                type="email" 
-                value={this.state.value} 
-                onChange={this.onChange}
-                placeholder="Email"
-                required />
-              <input
-                class="form-control" 
-                name="city" 
-                type="text" 
-                value={this.state.value} 
-                onChange={this.onChange}
-                placeholder="City"
-                required />
-              <select
-                class="form-control" 
-                name="state" 
-                id="state" 
-                onChange={this.onChange}
-                required>
+                <input
+                  class="form-control"
+                  name="title"
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.onChange}
+                  placeholder="Title"
+                  required />
+              </div>
+
+              <div class="line-2 input-box">
+                <div class="phoneNumbers">
+                  <input
+                    class="form-control mb-3"
+                    name="mobile"
+                    type="tel"
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    placeholder="Mobile number"/>
+                    
+                  <input
+                    class="form-control mb-3"
+                    name="office"
+                    type="tel"
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    placeholder="Office number"/>
+
+                  <input
+                    class="form-control mb-3"
+                    name="fax"
+                    type="tel"
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    placeholder="Fax number" />
+                </div>
+
+                <input
+                  class="form-control mb-3"
+                  name="email"
+                  type="email"
+                  value={this.state.value}
+                  onChange={this.onChange}
+                  placeholder="Email"
+                  required />
+                <input
+                  class="form-control mb-3"
+                  name="city"
+                  type="text"
+                  value={this.state.value}
+                  onChange={this.onChange}
+                  placeholder="City"
+                  required />
+
+                <select
+                  class="form-control"
+                  name="state"
+                  id="state"
+                  onChange={this.onChange}
+                  required>
                   <option value="" disabled selected>State</option>
                   <option value="AL">Alabama</option>
                   <option value="AK">Alaska</option>
@@ -158,56 +159,65 @@ onChange = (event) => {
                   <option value="WV">West Virginia</option>
                   <option value="WI">Wisconsin</option>
                   <option value="WY">Wyoming</option>
-              </select>
-              
-              <input
-                  class="form-control" 
-                  name="isa" 
-                  type="text" 
-                  value={this.state.value} 
+                </select>
+
+              </div>
+              <div class="line-3 input-box">
+                <input
+                  class="form-control mb-3"
+                  name="isa"
+                  type="text"
+                  value={this.state.value}
                   onChange={this.onChange}
                   placeholder="ISA Number (optional)" />
-              <input
-                  class="form-control" 
-                  name="isa" 
-                  type="text" 
-                  value={this.state.value} 
+                <input
+                  class="form-control"
+                  name="additional"
+                  type="text"
+                  value={this.state.value}
                   onChange={this.onChange}
                   placeholder="Additional Information (optional)" />
 
-              <button type="submit" class="btn">Submit</button>
+                <button type="submit" class="btn" id="submit-button">Submit</button>
+              </div>
             </form>
           </div>
 
+
+
           <div class="signature-container">
-            <div class="desktop-signature">
-              <Signature 
+            <div class="desktop-signature noselect">
+              <Signature
                 name={this.state.name}
                 title={this.state.title}
                 mobile={this.state.mobile}
                 office={this.state.office}
+                fax={this.state.fax}
                 email={this.state.email}
                 city={this.state.city}
                 state={this.state.state}
-                isa={this.state.isa} 
-                additional={this.state.additional}/>
+                isa={this.state.isa}
+                additional={this.state.additional} />
             </div>
 
             <div class="mobile-signature">
-              <MobileSignature 
-              name={this.state.name}
-              title={this.state.title}
-              mobile={this.state.mobile}
-              office={this.state.office}
-              email={this.state.email}
-              city={this.state.city}
-              state={this.state.state}
-              isa={this.state.isa} 
-              additional={this.state.additional}/>
+              <MobileSignature
+                name={this.state.name}
+                title={this.state.title}
+                mobile={this.state.mobile}
+                office={this.state.office}
+                fax={this.state.fax}
+                email={this.state.email}
+                city={this.state.city}
+                state={this.state.state}
+                isa={this.state.isa}
+                additional={this.state.additional} />
             </div>
           </div>
+
         </div>
-      </div>
+
+      </div >
     );
   }
 };
