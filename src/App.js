322d0df05a583f992502c98ace7757318ecedfe5
Greg <gkmarsella@gmail.com>
2020-05-20 15:47:17 -0400

    added address and zip code. Moved address info on its own line. Put website link on its own line

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ class App extends React.Component {
       office: '',
       fax: '',
       email: '',
+      address: '',
       city: '',
       state: '',
+      zipcode: '',
       isa: '',
       additional: '',
       copied: false,
@@ -141,6 +143,17 @@ class App extends React.Component {
                   <div className="mb-3">
                     <input
                       className="form-control"
+                      name="address"
+                      type="text"
+                      value={this.state.value}
+                      onChange={this.onChange}
+                      placeholder="Address"
+                      required />
+                  </div>
+
+                  <div className="mb-3">
+                    <input
+                      className="form-control"
                       name="city"
                       type="text"
                       value={this.state.value}
@@ -149,65 +162,78 @@ class App extends React.Component {
                       required />
                   </div>
 
-                  <select
-                    className="form-control"
-                    name="state"
-                    id="state"
-                    onChange={this.onChange}
-                    required>
-                    <option value="" defaultValue>State</option>
-                    <option value="AL">Alabama</option>
-                    <option value="AK">Alaska</option>
-                    <option value="AZ">Arizona</option>
-                    <option value="AR">Arkansas</option>
-                    <option value="CA">California</option>
-                    <option value="CO">Colorado</option>
-                    <option value="CT">Connecticut</option>
-                    <option value="DE">Delaware</option>
-                    <option value="DC">District Of Columbia</option>
-                    <option value="FL">Florida</option>
-                    <option value="GA">Georgia</option>
-                    <option value="HI">Hawaii</option>
-                    <option value="ID">Idaho</option>
-                    <option value="IL">Illinois</option>
-                    <option value="IN">Indiana</option>
-                    <option value="IA">Iowa</option>
-                    <option value="KS">Kansas</option>
-                    <option value="KY">Kentucky</option>
-                    <option value="LA">Louisiana</option>
-                    <option value="ME">Maine</option>
-                    <option value="MD">Maryland</option>
-                    <option value="MA">Massachusetts</option>
-                    <option value="MI">Michigan</option>
-                    <option value="MN">Minnesota</option>
-                    <option value="MS">Mississippi</option>
-                    <option value="MO">Missouri</option>
-                    <option value="MT">Montana</option>
-                    <option value="NE">Nebraska</option>
-                    <option value="NV">Nevada</option>
-                    <option value="NH">New Hampshire</option>
-                    <option value="NJ">New Jersey</option>
-                    <option value="NM">New Mexico</option>
-                    <option value="NY">New York</option>
-                    <option value="NC">North Carolina</option>
-                    <option value="ND">North Dakota</option>
-                    <option value="OH">Ohio</option>
-                    <option value="OK">Oklahoma</option>
-                    <option value="OR">Oregon</option>
-                    <option value="PA">Pennsylvania</option>
-                    <option value="RI">Rhode Island</option>
-                    <option value="SC">South Carolina</option>
-                    <option value="SD">South Dakota</option>
-                    <option value="TN">Tennessee</option>
-                    <option value="TX">Texas</option>
-                    <option value="UT">Utah</option>
-                    <option value="VT">Vermont</option>
-                    <option value="VA">Virginia</option>
-                    <option value="WA">Washington</option>
-                    <option value="WV">West Virginia</option>
-                    <option value="WI">Wisconsin</option>
-                    <option value="WY">Wyoming</option>
-                  </select>
+                  <div className="mb-3">
+                    <select
+                      className="form-control"
+                      name="state"
+                      id="state"
+                      onChange={this.onChange}
+                      required>
+                      <option value="" defaultValue>State</option>
+                      <option value="AL">Alabama</option>
+                      <option value="AK">Alaska</option>
+                      <option value="AZ">Arizona</option>
+                      <option value="AR">Arkansas</option>
+                      <option value="CA">California</option>
+                      <option value="CO">Colorado</option>
+                      <option value="CT">Connecticut</option>
+                      <option value="DE">Delaware</option>
+                      <option value="DC">District Of Columbia</option>
+                      <option value="FL">Florida</option>
+                      <option value="GA">Georgia</option>
+                      <option value="HI">Hawaii</option>
+                      <option value="ID">Idaho</option>
+                      <option value="IL">Illinois</option>
+                      <option value="IN">Indiana</option>
+                      <option value="IA">Iowa</option>
+                      <option value="KS">Kansas</option>
+                      <option value="KY">Kentucky</option>
+                      <option value="LA">Louisiana</option>
+                      <option value="ME">Maine</option>
+                      <option value="MD">Maryland</option>
+                      <option value="MA">Massachusetts</option>
+                      <option value="MI">Michigan</option>
+                      <option value="MN">Minnesota</option>
+                      <option value="MS">Mississippi</option>
+                      <option value="MO">Missouri</option>
+                      <option value="MT">Montana</option>
+                      <option value="NE">Nebraska</option>
+                      <option value="NV">Nevada</option>
+                      <option value="NH">New Hampshire</option>
+                      <option value="NJ">New Jersey</option>
+                      <option value="NM">New Mexico</option>
+                      <option value="NY">New York</option>
+                      <option value="NC">North Carolina</option>
+                      <option value="ND">North Dakota</option>
+                      <option value="OH">Ohio</option>
+                      <option value="OK">Oklahoma</option>
+                      <option value="OR">Oregon</option>
+                      <option value="PA">Pennsylvania</option>
+                      <option value="RI">Rhode Island</option>
+                      <option value="SC">South Carolina</option>
+                      <option value="SD">South Dakota</option>
+                      <option value="TN">Tennessee</option>
+                      <option value="TX">Texas</option>
+                      <option value="UT">Utah</option>
+                      <option value="VT">Vermont</option>
+                      <option value="VA">Virginia</option>
+                      <option value="WA">Washington</option>
+                      <option value="WV">West Virginia</option>
+                      <option value="WI">Wisconsin</option>
+                      <option value="WY">Wyoming</option>
+                    </select>
+                  </div>
+
+                  <div className="mb-3">
+                    <input
+                      className="form-control"
+                      name="zipcode"
+                      type="text"
+                      value={this.state.value}
+                      onChange={this.onChange}
+                      placeholder="Zipcode"
+                      required />
+                  </div>
 
                 </div>
                 <div className="line-3 input-box">
@@ -239,8 +265,10 @@ class App extends React.Component {
                     office={this.state.office}
                     fax={this.state.fax}
                     email={this.state.email}
+                    address={this.state.address}
                     city={this.state.city}
                     state={this.state.state}
+                    zipcode={this.state.zipcode}
                     isa={this.state.isa}
                     additional={this.state.additional} />
                 </div>
@@ -257,8 +285,10 @@ class App extends React.Component {
                   office={this.state.office}
                   fax={this.state.fax}
                   email={this.state.email}
+                  address={this.state.address}
                   city={this.state.city}
                   state={this.state.state}
+                  zipcode={this.state.zipcode}
                   isa={this.state.isa}
                   additional={this.state.additional} />
               </div>
@@ -282,8 +312,10 @@ class App extends React.Component {
                     office={this.state.office}
                     fax={this.state.fax}
                     email={this.state.email}
+                    address={this.state.address}
                     city={this.state.city}
                     state={this.state.state}
+                    zipcode={this.state.zipcode}
                     isa={this.state.isa}
                     additional={this.state.additional} />
                 </div>
